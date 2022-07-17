8db66fb90f79e0fec4071e23fd7b13a908db5020
Labib2003 <labib.salimi30@gmail.com>
2022-07-17 21:03:01 +0600

    action creators connected with ui buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,7 +1,11 @@
+import { useShoppingCart } from "../contexts/ShoppingCartContext";
 import { StoreItemProps } from "../types";
 
 const StoreItem = ({ id, title, price, description, category, image }: StoreItemProps) => {
-    const quantity = 0;
+    const { getItemQuantity, increaseQuantity, decreaseQuantity, deleteItem } = useShoppingCart();
+
+    const quantity = getItemQuantity(id);
+
     return (
         <div className="card bg-base-100 shadow-2xl">
             <figure className="h-1/2">
@@ -16,17 +20,20 @@ const StoreItem = ({ id, title, price, description, category, image }: StoreItem
                 <div className="card-actions mt-5">
                     {
                         quantity ?
-                            <div className="flex justify-around w-1/2 mx-auto">
-                                <button className="btn btn-primary rounded">
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 12H4" /></svg>
-                                </button>
-                                <p className="text-center text-xl font-medium">{quantity}</p>
-                                <button className="btn btn-primary rounded">
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
-                                </button>
+                            <div className="w-1/2 mx-auto">
+                                <div className="flex justify-around mb-3">
+                                    <button className="btn btn-primary rounded" onClick={() => decreaseQuantity(id)}>
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 12H4" /></svg>
+                                    </button>
+                                    <p className="text-center text-xl font-medium">{quantity}</p>
+                                    <button className="btn btn-primary rounded" onClick={() => increaseQuantity(id)}>
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
+                                    </button>
+                                </div>
+                                <button className="btn btn-error w-full rounded" onClick={() => deleteItem(id)}>Remove From Cart</button>
                             </div>
                             :
-                            <button className="btn btn-primary mx-auto rounded">Add To Cart</button>
+                            <button className="btn btn-primary mx-auto rounded" onClick={() => increaseQuantity(id)}>Add To Cart</button>
                     }
                 </div>
             </div>
